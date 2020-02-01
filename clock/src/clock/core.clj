a716641965c0840bab752620bd8ae2ffa31c4f8c
ahronl <aharon.levi@amobee.com>
2020-02-01 16:17:37 +0200

    pass minutes overload to hours

--- clock/src/clock/core.clj
+++ clock/src/clock/core.clj
@@ -1,4 +1,7 @@
 (ns clock.core)
 
+(defn to-string [c]
+ (str (:hour c) ":" (format "%02d" (:minute c))))
+
 (defn clock [h m]
- {:hour h :minute m})
+ {:hour (+ h (quot m 60)) :minute (mod m 60)})
--- clock/test/clock/core_test.clj
+++ clock/test/clock/core_test.clj
@@ -2,10 +2,13 @@
   (:require [clojure.test :refer :all]
             [clock.core :refer :all]))
 
-(defn print-clock [c]
- (str (:hour c) ":" (format "%02d" (:minute c))))
-
 (deftest clock-tests
-  (testing "set hour and minutes"
+  (testing "when set hour to 8 and minute to 0 should return 8:00"
     (let [clk (clock 8 0)]
-     (is (= "8:00" (print-clock clk))))))
+     (is (= "8:00" (to-string clk)))))
+  (testing "when set hour to 10 and minute to 11 should return 10:11"
+    (let [clk (clock 10 11)]
+     (is (= "10:11" (to-string clk)))))
+  (testing "when set hour 10 and minute to 61 should return 11:01"
+    (let [clk (clock 10 61)]
+     (is (= "11:01" (to-string clk))))))
